793a39ce6a1ed3b9623571a170c5af979315a2dd
Crypto-Pop-Collectibles <cryptopopcollectibles@gmail.com>
2023-02-21 16:43:50 -0800

    Updated Config for BigVGAN -Pytorch-Lion - Colossal Ai?

--- Configs/config.py
+++ Configs/config.py
@@ -5,11 +5,7 @@ fp16 = dict(
   # below are default values for grad scaler
 )
 
-# this is ok
-parallel = dict(
-    pipeline=1,
-    tensor=dict(size=2, mode='3d')
-)
+
 gradient_accumulation = 4
 clip_grad_norm = 1.0
 
