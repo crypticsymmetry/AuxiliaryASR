4cf318f239cb773f1ced821f524f0ec6cb2cfdb7
Crypto-Pop-Collectibles <cryptopopcollectibles@gmail.com>
2023-02-22 13:33:07 -0800

    Updated Config for BigVGAN -Pytorch-Lion + MultiGpu with Colossal AI

--- meldatasetBigVgan.py
+++ meldatasetBigVgan.py
@@ -68,10 +68,6 @@ class MelDataset(torch.utils.data.Dataset):
         wave_tensor = torch.from_numpy(wave).float()
         
 
-        if self.mel_basis is None or self.hann_window is None:
-            self.mel_basis, self.hann_window = librosa_mel_fn(
-                self.sr, MEL_PARAMS['n_fft'], MEL_PARAMS['num_mels'],
-                MEL_PARAMS['fmin'], MEL_PARAMS['fmax'])
 
         mel_tensor = librosa.feature.melspectrogram(
             y=wave_tensor.cpu().numpy(),
