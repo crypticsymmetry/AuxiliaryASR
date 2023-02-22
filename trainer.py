667112a287bcf70bcd17d498c8aee09e9344aa90
Crypto-Pop-Collectibles <cryptopopcollectibles@gmail.com>
2023-02-21 19:09:06 -0800

    Updated Config for BigVGAN -Pytorch-Lion

--- trainer.py
+++ trainer.py
@@ -22,7 +22,7 @@ from utils import *
 
 class Trainer(object):
     def __init__(self,
-                 model.module=None,
+                 model=None,
                  criterion=None,
                  optimizer=None,
                  scheduler=None,
@@ -36,7 +36,7 @@ class Trainer(object):
 
         self.steps = initial_steps
         self.epochs = initial_epochs
-        self.model.module = model.module
+        self.model = model.module
         self.criterion = criterion
         self.optimizer = optimizer
         self.scheduler = scheduler
@@ -59,7 +59,7 @@ class Trainer(object):
             "steps": self.steps,
             "epochs": self.epochs,
         }
-        state_dict["model.module"] = self.model.module.state_dict()
+        state_dict["model"] = self.model.state_dict()
 
         if not os.path.exists(os.path.dirname(checkpoint_path)):
             os.makedirs(os.path.dirname(checkpoint_path))
@@ -70,11 +70,11 @@ class Trainer(object):
 
         Args:
             checkpoint_path (str): Checkpoint path to be loaded.
-            load_only_params (bool): Whether to load only model.module parameters.
+            load_only_params (bool): Whether to load only model parameters.
 
         """
         state_dict = torch.load(checkpoint_path, map_location="cpu")
-        self._load(state_dict["model.module"], self.model.module)
+        self._load(state_dict["model"], self.model)
 
         if not load_only_params:
             self.steps = state_dict["steps"]
@@ -85,34 +85,34 @@ class Trainer(object):
             state_dict["scheduler"].update(**self.config.get("scheduler_params", {}))
             self.scheduler.load_state_dict(state_dict["scheduler"])
 
-    def _load(self, states, model.module, force_load=True):
-        model.module_states = model.module.state_dict()
+    def _load(self, states, model, force_load=True):
+        model_states = model.state_dict()
         for key, val in states.items():
             try:
-                if key not in model.module_states:
+                if key not in model_states:
                     continue
                 if isinstance(val, nn.Parameter):
                     val = val.data
 
-                if val.shape != model.module_states[key].shape:
+                if val.shape != model_states[key].shape:
                     self.logger.info("%s does not have same shape" % key)
-                    print(val.shape, model.module_states[key].shape)
+                    print(val.shape, model_states[key].shape)
                     if not force_load:
                         continue
 
-                    min_shape = np.minimum(np.array(val.shape), np.array(model.module_states[key].shape))
+                    min_shape = np.minimum(np.array(val.shape), np.array(model_states[key].shape))
                     slices = [slice(0, min_index) for min_index in min_shape]
-                    model.module_states[key][slices].copy_(val[slices])
+                    model_states[key][slices].copy_(val[slices])
                 else:
-                    model.module_states[key].copy_(val)
+                    model_states[key].copy_(val)
             except:
                 self.logger.info("not exist :%s" % key)
                 print("not exist ", key)
 
     @staticmethod
-    def get_gradient_norm(model.module):
+    def get_gradient_norm(model):
         total_norm = 0
-        for p in model.module.parameters():
+        for p in model.parameters():
             param_norm = p.grad.data.norm(2)
             total_norm += param_norm.item() ** 2
 
@@ -155,12 +155,12 @@ class Trainer(object):
         self.optimizer.zero_grad()
         batch = [b.to(self.device) for b in batch]
         text_input, text_input_length, mel_input, mel_input_length = batch
-        mel_input_length = mel_input_length // (2 ** self.model.module.module.n_down)
-        future_mask = self.model.module.get_future_mask(
-            mel_input.size(2)//(2**self.model.module.n_down), unmask_future_steps=0).to(self.device)
-        mel_mask = self.model.module.length_to_mask(mel_input_length)
-        text_mask = self.model.module.length_to_mask(text_input_length)
-        ppgs, s2s_pred, s2s_attn = self.model.module(
+        mel_input_length = mel_input_length // (2 ** self.model.n_down)
+        future_mask = self.model.get_future_mask(
+            mel_input.size(2)//(2**self.model.n_down), unmask_future_steps=0).to(self.device)
+        mel_mask = self.model.length_to_mask(mel_input_length)
+        text_mask = self.model.length_to_mask(text_input_length)
+        ppgs, s2s_pred, s2s_attn = self.model(
             mel_input, src_key_padding_mask=mel_mask, text_input=text_input)
         
         loss_ctc = self.criterion['ctc'](ppgs.log_softmax(dim=2).transpose(0, 1),
@@ -173,7 +173,7 @@ class Trainer(object):
 
         loss = loss_ctc + loss_s2s
         loss.backward()
-        torch.nn.utils.clip_grad_value_(self.model.module.parameters(), 5)
+        torch.nn.utils.clip_grad_value_(self.model.parameters(), 5)
         self.optimizer.step()
         self.scheduler.step()
         return {'loss': loss.item(),
@@ -182,7 +182,7 @@ class Trainer(object):
 
     def _train_epoch(self):
         train_losses = defaultdict(list)
-        self.model.module.train()
+        self.model.train()
         for train_steps_per_epoch, batch in enumerate(tqdm(self.train_dataloader, desc="[train]"), 1):
             losses = self.run(batch)
             for key, value in losses.items():
@@ -194,18 +194,18 @@ class Trainer(object):
 
     @torch.no_grad()
     def _eval_epoch(self):
-        self.model.module.eval()
+        self.model.eval()
         eval_losses = defaultdict(list)
         eval_images = defaultdict(list)
         for eval_steps_per_epoch, batch in enumerate(tqdm(self.val_dataloader, desc="[eval]"), 1):
             batch = [b.to(self.device) for b in batch]
             text_input, text_input_length, mel_input, mel_input_length = batch
-            mel_input_length = mel_input_length // (2 ** self.model.module.n_down)
-            future_mask = self.model.module.get_future_mask(
-                mel_input.size(2)//(2**self.model.module.n_down), unmask_future_steps=0).to(self.device)
-            mel_mask = self.model.module.length_to_mask(mel_input_length)
-            text_mask = self.model.module.length_to_mask(text_input_length)
-            ppgs, s2s_pred, s2s_attn = self.model.module(
+            mel_input_length = mel_input_length // (2 ** self.model.n_down)
+            future_mask = self.model.get_future_mask(
+                mel_input.size(2)//(2**self.model.n_down), unmask_future_steps=0).to(self.device)
+            mel_mask = self.model.length_to_mask(mel_input_length)
+            text_mask = self.model.length_to_mask(text_input_length)
+            ppgs, s2s_pred, s2s_attn = self.model(
                 mel_input, src_key_padding_mask=mel_mask, text_input=text_input)
             loss_ctc = self.criterion['ctc'](ppgs.log_softmax(dim=2).transpose(0, 1),
                                           text_input, mel_input_length, text_input_length)
