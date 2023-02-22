d0302a0206bfdceb2dfa908f835c4a12df685c18
Crypto-Pop-Collectibles <cryptopopcollectibles@gmail.com>
2023-02-21 20:55:23 -0800

    Updated Config for BigVGAN -Pytorch-Lion + MultiGpu with Colossal AI

--- Configs/config.py
+++ Configs/config.py
@@ -1,16 +1,14 @@
 from colossalai.amp import AMP_TYPE
 
-fp16 = dict(
-  mode=AMP_TYPE.TORCH
-  # below are default values for grad scaler
-)
+fp16 = dict(mode=AMP_TYPE.NAIVE)
 
 parallel = dict(
-    tensor=dict(size=2, mode='3d')
+    pipeline=1,
+    tensor=dict(size=2, mode='1d'),    # for the current model implementation, mode can only be 1D or None
 )
 
-gradient_accumulation = 4
 clip_grad_norm = 1.0
+gradient_accumulation = 2
 
 rank=0
 world_size=1
