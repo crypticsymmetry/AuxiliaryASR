d57d9075e128d522fcf1a039acbcc18806d586a9
Crypto-Pop-Collectibles <cryptopopcollectibles@gmail.com>
2023-02-22 17:42:32 -0800

    Updated Config for BigVGAN -Pytorch-Lion + MultiGpu with Colossal AI

--- meldatasetBigVgan.py
+++ meldatasetBigVgan.py
@@ -56,30 +56,72 @@ class MelDataset(torch.utils.data.Dataset):
         self.mean, self.std = -4, 4
         
         self.g2p = G2p()
+        self.MAX_WAV_VALUE = 32768.0
+
     
-    def mel_spectrogram(waveform, sr):
-        # Compute Mel spectrogram
-        mel_spect = librosa.feature.melspectrogram(y=waveform, sr=sr, **MEL_PARAMS)
-        
-        # Convert to log scale (dB) using the peak power as reference
-        log_mel_spect = librosa.power_to_db(mel_spect, ref=np.max)
 
-        # Normalize the spectrogram
-        mean = np.mean(log_mel_spect)
-        std = np.std(log_mel_spect)
-        norm_mel_spect = (log_mel_spect - mean) / std
-
-        return norm_mel_spect
 
     def __len__(self):
         return len(self.data_list)
 
     def __getitem__(self, idx):
+
+        def melSpectrogram(y, n_fft=1024, num_mels=100, sampling_rate=24000, hop_size=256, win_size=1024, fmin=0, fmax=12000, center=False):
+            
+            def dynamic_range_compression(x, C=1, clip_val=1e-5):
+                return np.log(np.clip(x, a_min=clip_val, a_max=None) * C)
+
+
+            def dynamic_range_decompression(x, C=1):
+                return np.exp(x) / C
+
+
+            def dynamic_range_compression_torch(x, C=1, clip_val=1e-5):
+                return torch.log(torch.clamp(x, min=clip_val) * C)
+
+
+            def dynamic_range_decompression_torch(x, C=1):
+                return torch.exp(x) / C
+            
+            def spectral_normalize_torch(magnitudes):
+                output = dynamic_range_compression_torch(magnitudes)
+                return output
+
+
+            def spectral_de_normalize_torch(magnitudes):
+                output = dynamic_range_decompression_torch(magnitudes)
+                return output
+            if torch.min(y) < -1.:
+                print('min value is ', torch.min(y))
+            if torch.max(y) > 1.:
+                print('max value is ', torch.max(y))
+
+            global mel_basis, hann_window
+            if fmax not in mel_basis:
+                mel = librosa.fiters.mel(sampling_rate, n_fft, num_mels, fmin, fmax)
+                mel_basis[str(fmax)+'_'+str(y.device)] = torch.from_numpy(mel).float().to(y.device)
+                hann_window[str(y.device)] = torch.hann_window(win_size).to(y.device)
+
+            y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
+            y = y.squeeze(1)
+
+            # complex tensor as default, then use view_as_real for future pytorch compatibility
+            spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[str(y.device)],
+                            center=center, pad_mode='reflect', normalized=False, onesided=True, return_complex=True)
+            spec = torch.view_as_real(spec)
+            spec = torch.sqrt(spec.pow(2).sum(-1)+(1e-9))
+
+            spec = torch.matmul(mel_basis[str(fmax)+'_'+str(y.device)], spec)
+            spec = spectral_normalize_torch(spec)
+
+            return spec
         data = self.data_list[idx]
         wave, text_tensor, speaker_id = self._load_tensor(data)
-        wave_tensor = wave.numpy()
+        wave = wave / self.MAX_WAV_VALUE
+
         
-        mel_tensor = self.mel_spectrogram(wave_tensor)
+        wave_tensor = torch.FloatTensor(wave)
+        mel_tensor = melSpectrogram(wave_tensor)
 
         if (text_tensor.size(0)+1) >= (mel_tensor.size(1) // 3):
             mel_tensor = F.interpolate(
