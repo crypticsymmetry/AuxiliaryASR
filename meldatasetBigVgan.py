9c763864304e18c68236362ec85b85f702dbc5c8
Crypto-Pop-Collectibles <cryptopopcollectibles@gmail.com>
2023-02-22 17:06:50 -0800

    Updated Config for BigVGAN -Pytorch-Lion + MultiGpu with Colossal AI

--- meldatasetBigVgan.py
+++ meldatasetBigVgan.py
@@ -58,7 +58,7 @@ class MelDataset(torch.utils.data.Dataset):
         self.g2p = G2p()
     
     def mel_spectrogram(waveform, sr):
-        waveform = waveform.numpy()
+        waveform = waveform.detach().numpy()
         # Compute Mel spectrogram
         mel_spect = librosa.feature.melspectrogram(y=waveform, sr=sr, **MEL_PARAMS)
         
